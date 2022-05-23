ab8f9475ddda90407a4e6c89c336483c1f1d0ca7
Dave-GP <85894890+Dave-GP@users.noreply.github.com>
2022-05-23 00:13:03 -0600

    1st

---
+++ ChivoFlixWeb/Controllers/CategoriasController.cs
@@ -0,0 +1,84 @@
+﻿using ChivoFlixWeb.Models;
+using ChivoFlixWeb.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChivoFlixWeb.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly CHIVOFLIXContext _context;
+        public CategoriasController(CHIVOFLIXContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Listado()
+        {
+            List<Generos> listGeneros;
+            listGeneros = (from g in _context.Generos
+                           select new Generos
+                           {
+                               IdGeneros = g.IdGeneros,
+                               Nombre= g.Nombre
+                           }).ToList();
+            return View(listGeneros);
+        }
+
+        public IActionResult NuevaPelicula()
+        {
+            List<Generos> listadoGeneros = null;
+
+            listadoGeneros = (from g in _context.Generos
+                              select new Generos()
+                              {
+                                  IdGeneros = g.IdGeneros,
+                                  Nombre = g.Nombre
+                              }).ToList();
+
+            List<SelectListItem> items = listadoGeneros.ConvertAll(g =>
+            {
+                return new SelectListItem()
+                {
+                    Text = g.Nombre.ToString(),
+                    Value = g.IdGeneros.ToString(),
+                    Selected = false,
+                };
+            });
+
+            ViewBag.items = items;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult NuevaCategoria(GenerosVM model)
+        {
+            var db = _context;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var oGeneros = new Generos
+                    {
+                        IdGeneros = model.IdGeneros,
+                        Nombre = model.Nombre,
+                       
+                       
+                    };
+                    db.Generos.Add(oGeneros);
+                    db.SaveChanges();
+                    return Redirect("~/Categorias/Listado/");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ChivoFlixWeb/Models/ViewModels/GenerosVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChivoFlixWeb.Models.ViewModels
+{
+    public class GenerosVM
+    {
+        public GenerosVM()
+        {
+           
+        }
+        [Key]
+        public int IdGeneros { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido!")]
+        [Display(Name = "Ingrese categoria")]
+        public string Nombre { get; set; }
+
+        public virtual ICollection<Peliculas> Peliculas { get; set; }
+
+
+    }
+}
