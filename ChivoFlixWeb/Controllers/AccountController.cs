fae9983d5c7d9cc7856eb3552465991375996596
dsalazar31 <alejandrodanielsalazar@gmail.com>
2022-05-27 22:15:21 -0600

    actualizar

--- ChivoFlixWeb/Controllers/AccountController.cs
+++ ChivoFlixWeb/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using ChivoFlixWeb.Models;
+using ChivoFlixWeb.Models.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,6 +37,64 @@ namespace ChivoFlixWeb.Controllers
             HttpContext.Session.Clear();
             HttpContext.Session.Remove("username");
             return RedirectToAction("Login", "Account");
+        }
+
+        public IActionResult Suscripcion()
+        {
+            var listadoPlanes = (from planes in _contex.Planes join duracion in _contex.DuracionPlanes
+                             on planes.IdDuracionPlanes equals duracion.IdDuracionPlanes
+                             select new PlanesDuracionVM
+                             {
+                                 IdDuracionPlanes = planes.IdPlanes,
+                                 Plann = planes.Plann,
+                                 PrecioPlan = planes.PrecioPlan,
+                                 Nombre = duracion.Nombre,
+                                 IdPlanes = planes.IdPlanes
+
+                             }).ToList();
+
+            List<SelectListItem> itemss = listadoPlanes.ConvertAll(planes =>
+            {
+                return new SelectListItem()
+                {
+                    Text = planes.Nombre.ToString(),
+                    Value = planes.IdPlanes.ToString(),
+                    Selected = false,
+                };
+            });
+
+            ViewBag.items = itemss;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Suscripcion(UsuarioVM model)
+        {
+            var db = _contex;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var oUsuario = new Usuarios
+                    {
+                        Username = model.Username,
+                        Email = model.Email,
+                        Password = model.Password,
+                        Imagen = model.Imagen,
+                        IdRol = 0,
+                        IdPlanes = model.IdPlanes,
+                        Perfiles = 4,
+                    };
+                    db.Usuarios.Add(oUsuario);
+                    db.SaveChanges();
+                    return Redirect("~/Account/Login/");
+                }
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
